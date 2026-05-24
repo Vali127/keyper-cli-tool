59da9cbb1e409bd901e3ff95e8ff5d960e7eed1d
Vali127 <mh.valimbavaka@gmail.com>
2026-05-24 22:51:03 +0300

    feat : interface improvment

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { deleteCommand } from './commands/delete.js';
 import { generateCommand } from './commands/generate.js';
 import select from '@inquirer/select';
 import figlet from "figlet";
+import { readFileSync } from 'fs';
+
+figlet.parseFont('3d', readFileSync('./src/assets/ANSI Regular.flf', 'utf8'));
 
 program
     .name('keyper')
@@ -20,13 +23,14 @@ program.addCommand(getCommand);
 program.addCommand(deleteCommand);
 program.addCommand(generateCommand);
 
-console.log(pc.magenta(figlet.textSync('Keyper', { font: 'Standard' })));
+console.log(pc.gray("\nstarting...\n"));
+console.log(pc.white(figlet.textSync('Keyper', { font: '3d' })));
 console.log(pc.gray("A lightweight and secure CLI password manager built with Node.js.\n"))
 //Menu if there are no given arguments
 if (process.argv.length === 2) {
     try {
         const action = await select({
-            message: pc.magenta('What do you want to do?'),
+            message: pc.bold('What do you want to do?'),
             choices: [
                 { name: 'Add a password',      value: 'add' },
                 { name: 'List passwords',      value: 'list' },
@@ -36,7 +40,7 @@ if (process.argv.length === 2) {
             ],
             theme: {
                 style: {
-                    highlight: (text) => pc.bold(pc.cyan(text)),
+                    highlight: (text) => pc.bold(pc.blue(text)),
                 },
             },
         });
